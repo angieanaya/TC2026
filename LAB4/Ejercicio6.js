fe3388cbca4fdb03bfa0aa5d66294c7fce79361e
angie_anaya <angi_anaya@MacBook-Pro-de-Angie.local>
2020-02-25 06:26:01 -0600

    Lab4 final

--- LAB4/Ejercicio6.js
+++ LAB4/Ejercicio6.js
@@ -1,28 +1,43 @@
-function IMC(){
-	let peso = prompt("Ingresa tu peso actual en kg: ");
-	let estatura = prompt("Ingresa tu estatura en metros: ")
-	
-	let imc = ((peso)/Math.pow(estatura, 2)).toFixed(3);
+// Creates a new list item when clicking on the "ADD TASK" button
+function addTask() {
+  //creates a <li> tag in which the input will be stored
+  let li = document.createElement("li");
+  //retrieves the text entered by the user
+  let inputValue = document.getElementById("taskInput").value;
+  let t = document.createTextNode(inputValue);
+  li.appendChild(t);
 
-	if (imc>=0 && imc<18.5)
-    {
-        alert("Tu IMC es de: " + imc +", estás por debajo del peso saludable.\n");
+  //if the placeholder is empty it reminds the user to write something
+  if (inputValue === '') {
+    alert("Enter a task first please :)");
+  } else {
+    document.getElementById("myTasks").appendChild(li);
+  }
+  //adds the task entered to the bottom of the list set in the html file
+  document.getElementById("taskInput").value = "";
+
+  //prints the close button in the right place
+  let span = document.createElement("SPAN");
+  let txt = document.createTextNode("x");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  //adds functionality to the close button append to each list item
+  let close = document.getElementsByClassName("close");
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      let div = this.parentElement;
+      div.style.display = "none";
     }
-    else if (imc>=18.5 && imc<25)
-    {
-        alert("Tu IMC es de: " + imc + ", estás dentro del rango de peso saludable.\n");
-    }
-  	else if (imc>=25 && imc<30)
-    {
-        alert("Tu IMC es de: " + imc + ", tienes sobrepeso.\n");
-    }
-    else{
-        alert("Tu IMC es de: " + imc + ", estás dentro del rango de obesidad.\n");
-        if (imc>=30 && imc<35)
-            alert("Tienes obesidad nivel 1.\n");
-        else if (imc>=35 && imc<40)
-            alert("Tienes obesidad severa.\n");
-        else
-            alert("Tienes obesidad nivel 3, osea obesidad morvida.\n");
-    }
+  }
+
+    //crosses out the tasks when clicked on them
+    let list = document.querySelector('ul');
+    list.addEventListener('click', function(ev) {
+        if (ev.target.tagName === 'LI') {
+            ev.target.classList.add('checked');
+        }
+    });
 }
