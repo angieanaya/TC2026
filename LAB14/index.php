7f0d3d55d56d323024bbe42b850fa35f249e8105
angie_anaya <angi_anaya@MacBook-Pro-de-Angie.local>
2020-03-31 11:47:58 -0600

    Agregar funcionalidad LAB14

---
+++ LAB14/index.php
@@ -0,0 +1,11 @@
+<?php
+require_once("util.php");
+
+include("header.html"); 
+include("body.html"); 
+
+//echo consultarLibros();
+
+include("preguntas.html"); 
+include("footer.html"); 
+?>
--- LAB14/util.php
+++ LAB14/util.php
@@ -1,45 +1,22 @@
-function connectDb(){
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "dbname";
-
-	$con = mysqli_connect($servername, $username, $password, $dbname);
-
-	if(!$con){
-		die("Connection failed: " . mysqli_conect_error());
-	}
-
-	return $con;
-}
-
-function closeDb($mysql){
-	mysqli_close($mysql);
-}
-
-function getObjects(){
-	$conn = connectDb();
-	$sql = "SELECT name, unit, quantity, price, country FROM Fruit";
-	$result = mysqli_query($con, $sql);
-	closeDb($con)
-	return $result;
-}
-
 <?php
-	require_once "util.php";
+//Función para conectarnos a la base de datos
+function conectDB(){
+  $conexion_db = mysqli_connect("localhost","root","","LAB14");
+      if ($conexion_fb == NULL) {
+          die("No se pudo conectar con la base de datos");
+      }
+      return $conexion_db;
+}
 
-	$result = getObjects();
+//Función para desconectarnos de la base de datos
+function closeDB($conexion_db){
+   mysqli_close($conexion_db);
+}
 
-	if(mysql_num_rows($result) > 0){
-		while ($row = mysqli_fetch_assoc($result)) {
-			echo "<tr>";
-			echo "<td>" . $row[""] . "<td>";
-			echo "<td>" . $row[""] . "<td>";
-			echo "<td>" . $row[""] . "<td>";
-			echo "<td>" . $row[""] . "<td>";
-			echo "<td>" . $row[""] . "<td>";
-			echo "<tr>";
-		}
-	}
+/*function consultarLibros(){
+  $conexion_db = conectDB();
+
+  closeDB($conexion_db);
+}*/
 ?>
 
