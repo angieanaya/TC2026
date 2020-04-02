20e2ec6624c754ce106b6134b6e11c20fac01d85
angie_anaya <angianaya17@gmail.com>
2020-04-02 02:07:33 -0600

    Agregar consultar y select

--- LAB14/index.php
+++ LAB14/index.php
@@ -4,7 +4,19 @@ require_once("util.php");
 include("header.html"); 
 include("body.html"); 
 
-//echo consultarLibros();
+/*if (isset($_POST["idGenero"])) {
+      $idGenero = htmlspecialchars($_POST["idGenero"]);
+  } else {
+      $idGenero = "";
+  }
+
+ if (isset($_POST["idAutor"])) {
+      $idAutor = htmlspecialchars($_POST["idAutor"]);
+  } else {
+      $idAutor = "";
+}*/
+
+echo consultar("","","");
 
 include("preguntas.html"); 
 include("footer.html"); 
--- LAB14/util.php
+++ LAB14/util.php
@@ -1,8 +1,9 @@
 <?php
+
 //Función para conectarnos a la base de datos
 function conectDB(){
   $conexion_db = mysqli_connect("localhost","root","","LAB14");
-      if ($conexion_fb == NULL) {
+      if ($conexion_db == NULL) {
           die("No se pudo conectar con la base de datos");
       }
       return $conexion_db;
@@ -13,10 +14,60 @@ function closeDB($conexion_db){
    mysqli_close($conexion_db);
 }
 
-/*function consultarLibros(){
+function consultar($Titulo="", $idAutor="", $idGenero=""){
   $conexion_db = conectDB();
+  
+  $resultado =  "<table><thead><tr><th>Título</th><th>Autor</th><th>Género</th></tr></thead>";
+    
+  $consulta = 'Select Titulo, idGenero, idAutor From Libros';
+  
+  if ($Titulo != "") {
+        $consulta .= " AND titulo=".$Titulo;
+  }
+  
+  if ($idGenero != "") {
+        $consulta .= " AND idGenero=".$idGenero;
+  }
+
+  if ($idAutor != "") {
+        $consulta .= " AND idAutor=".$idAutor;
+  }
+      
+  $resultados = $conexion_db->query($consulta);  
+  while ($row = mysqli_fetch_array($resultados, MYSQLI_BOTH)) {
+        $resultado .= "<tr>";
+        $resultado .= "<td>".$row['Titulo']."</td>"; //Se puede usar el índice de la consulta
+        $resultado .= "<td>".$row['idAutor']."</td>"; //o el nombre de la columna
+        $resultado .= "<td>".$row['idGenero']."</td>";
+        $resultado .= "</tr>";
+  }
+    
+  mysqli_free_result($resultados); //Liberar la memoria
 
   closeDB($conexion_db);
-}*/
+
+        
+  $resultado .= "</tbody></table>";
+  return $resultado;
+}
+//Crea un select con los datos de una consulta
+  //@param $id: Campo en una tabla que contiene el id
+  //@param $columna_descripcion: Columna de una tabla con una descripción
+  //@param $tabla: La tabla a consultar en la bd
+  function crear_select($id, $columna_descripcion, $tabla) {
+    $conexion_bd = conectDB();  
+      
+    $resultado = '<div class="input-field"><select name="'.$tabla.'"><option value="" disabled selected>Selecciona una opción</option>';
+            
+    $consulta = "SELECT $id as id , $columna_descripcion as colum FROM $tabla";
+    $resultados = $conexion_bd->query($consulta);
+    while ($row = mysqli_fetch_array($resultados, MYSQLI_BOTH)) {
+       $resultado .= '<option value="'.$row["id"].'">'.$row["colum"].'</option>';
+    }
+        
+    closeDB($conexion_bd);
+    $resultado .=  '</select><label>'.$tabla.'...</label></div>';
+    return $resultado;
+  }
 ?>
 
